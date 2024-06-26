d57d447d390db4b8a0fd2a3826156247abdd57e2
samyak2003-coder <samyaksinghal2003@gmail.com>
2024-06-26 21:53:54 +0530

    vidscomponent css adjusted

--- app/(user)/physical-fitness/VidsComponent.tsx
+++ app/(user)/physical-fitness/VidsComponent.tsx
@@ -15,23 +15,21 @@ interface VidPage {
 }
 
 const IndividualVid = (props: VidComp) => {
-    return(
-        <>
-            <div className="mb-4">
+    return (
+        <div className="mb-8 flex-col lg:flex-row lg:items-start flex px-4 lg:px-32">
+            <div className="lg:w-1/2 lg:pr-4">
                 <h2 className="text-white text-xl md:text-2xl lg:text-3xl font-bold">{props.title}</h2>
-                <br />
-                <p>{props.desc}</p>
-                <br />
-                <div className="aspect-w-16 aspect-h-9">
-                    <iframe src={props.videoUrl} allowFullScreen className="w-full aspect-video" />
-                </div>
+                <p className="mt-2">{props.desc}</p>
             </div>
-        </>
-    )
+            <div className="mt-4 lg:mt-0 lg:w-1/2">
+                <iframe src={props.videoUrl} allowFullScreen className="w-full aspect-video" />
+            </div>
+        </div>
+    );
 }
 
 const VidComponent = (props: VidPage) => {
-    return(
+    return (
         <div className="text-white mt-[25%] md:mt-[15%] lg:mt-[8%] mb-[5%] p-8">
             <div className="font-bold text-xl md:text-2xl lg:text-3xl xl:text-4xl text-center">
                 <h3>{props.miniTitle.toUpperCase()}</h3> <br />
@@ -43,7 +41,7 @@ const VidComponent = (props: VidPage) => {
                 <IndividualVid key={index} {...vid} />
             ))}
         </div>
-    )
+    );
 }
 
 export default VidComponent;

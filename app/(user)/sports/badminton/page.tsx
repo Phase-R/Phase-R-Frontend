8a477382eebbf2b9d76721eb38250ee2eec4b0d7
Nihal T M <nihaltmdev@gmail.com>
2024-07-13 10:46:49 +0530

    Small image fixes in the badminton page and the main football page

--- app/(user)/sports/badminton/page.tsx
+++ app/(user)/sports/badminton/page.tsx
@@ -2,14 +2,13 @@ import React from "react";
 import PageHeader from "@/app/(user)/PageHeader";
 import SubPageSection from "@/app/(user)/sports/cricket/SubPageSection";
 
-export default function Cricket() {
+export default function Badminton() {
     return (
         <div>                       
             <PageHeader headerColor="bg-blue-500" headerHeading="Badminton" headerDesc="Badminton is a fast-paced sport requiring agility, precision, and strategy. Players need exceptional skill to dominate with powerful smashes, deft net shots, and tactical positioning." bgImage={'/sports_page/badminton_bg.png'} headerSubtextArray={["Strategy", "Patience", "Resilience"]} />
-            <SubPageSection imageUrl="/sports_page/cricket_page_batting.png" dispText="Batting" />
-            <SubPageSection imageUrl="/sports_page/cricket_page_bowling.png" dispText="Bowling" />
-            <SubPageSection imageUrl="/sports_page/cricket_page_keeping.png" dispText="Wicket Keeping" />
-            <SubPageSection imageUrl="/sports_page/cricket_page_fielding.png" dispText="Fielding" />
-        </div>
+            <SubPageSection imageUrl="/sports_page/badminton_page_fundamentals.png" dispText="FUNDAMENTALS, MOVEMENT AND NETPLAY" />
+            <SubPageSection imageUrl="/sports_page/badminton_page_attacking.png" dispText="ATTACKING AND DOWNWARD SHOTS" />
+            <SubPageSection imageUrl="/sports_page/badminton_page_defense.png" dispText="DEFENSE" />
+            </div>
     )
 }

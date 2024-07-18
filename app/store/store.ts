328863fbff4387e8454de60c4a12a763b8befb39
Nihal T M <nihaltmdev@gmail.com>
2024-07-18 23:49:53 +0530

    basic auth store zustand

--- app/store/store.ts
+++ app/store/store.ts
@@ -1,13 +1,24 @@
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware';
 
 interface AuthState {
   isAuthenticated: boolean;
   setIsAuthenticated: (value: boolean) => void;
+  getIsAuthenticated: () => boolean;
 }
 
-const useAuthStore = create<AuthState>((set) => ({
-  isAuthenticated: false,
-  setIsAuthenticated: (value) => set({ isAuthenticated: value }),
-}));
+const useAuthStore = create<AuthState>()(
+  persist(
+    (set, get) => ({
+      isAuthenticated: false,
+      setIsAuthenticated: (value) => set({ isAuthenticated: value }),
+      getIsAuthenticated: () => get().isAuthenticated,
+    }),
+    {
+      name: 'auth-storage',
+      storage: createJSONStorage(() => sessionStorage),   
+    }
+  )
+);
 
 export { useAuthStore };

a0741eea345c0df302ab2fc3a509bcbb79428a5b
Siddhartha Aakash Rao <siddy0041@gmail.com>
2024-08-07 19:11:59 +0530

    changes to vidscomponent for adding goal tracking feature

--- app/(user)/physical-fitness/VidsComponent.tsx
+++ app/(user)/physical-fitness/VidsComponent.tsx
@@ -1,31 +1,17 @@
 import React from "react";
 
 interface VidComp {
-    title: string,
-    desc: string,
-    videoUrl: string,
+    title: string;
+    desc: string;
+    videoUrl: string;
 }
 
 interface VidPage {
-    miniTitle: string,
-    pageTitle: string,
-    pageTitleColor: string,
-    pageDesc: string,
-    videos: VidComp[],
-}
-
-const IndividualVid = (props: VidComp) => {
-    return (
-        <div className="mb-8 flex-col lg:flex-row lg:items-start flex px-4 lg:px-32">
-            <div className="lg:w-1/2 lg:pr-4">
-                <h2 className="text-white text-xl md:text-2xl lg:text-3xl font-bold">{props.title}</h2>
-                <p className="mt-2">{props.desc}</p>
-            </div>
-            <div className="mt-4 lg:mt-0 lg:w-1/2">
-                <iframe src={props.videoUrl} allowFullScreen className="w-full aspect-video" />
-            </div>
-        </div>
-    );
+    miniTitle: string;
+    pageTitle: string;
+    pageTitleColor: string;
+    pageDesc: string;
+    videos: VidComp[];
 }
 
 const VidComponent = (props: VidPage) => {
@@ -38,7 +24,20 @@ const VidComponent = (props: VidPage) => {
             <p className="text-lg md:text-xl lg:text-2xl">{props.pageDesc}</p> <br />
 
             {props.videos.map((vid, index) => (
-                <IndividualVid key={index} {...vid} />
+                <div key={index} className="mb-8 flex-col lg:flex-row lg:items-start flex px-4 lg:px-32">
+                    <div className="lg:w-1/2 lg:pr-4">
+                        <h2 className="text-white text-xl md:text-2xl lg:text-3xl font-bold">{vid.title}</h2>
+                        <p className="mt-2">{vid.desc}</p>
+                    </div>
+                    <div className="mt-4 lg:mt-0 lg:w-1/2 flex flex-col items-center">
+                        <iframe src={vid.videoUrl} allowFullScreen className="w-full aspect-video" />
+                        <button 
+                            className="px-6 py-2 mt-4 bg-blue-500 text-white rounded-full hover:bg-blue-700"
+                        >
+                            Mark as Complete
+                        </button>
+                    </div>
+                </div>
             ))}
         </div>
     );
